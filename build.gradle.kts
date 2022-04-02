2d952be385cbd1bb64afc9f585964e828336926a
Jorge Cabrera <jorgecabrera@MacBook-Air-de-Jorge.local>
2022-04-01 21:56:58 -0300

    adding shadow jar task

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,18 @@ repositories {
     mavenCentral()
 }
 
+buildscript {
+    repositories {
+        maven("https://plugins.gradle.org/m2/")
+    }
+    dependencies {
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:9.2.1") {
+            exclude("org.jetbrains.kotlin:kotlin-reflect")
+        }
+        classpath("com.github.jengelman.gradle.plugins:shadow:5.2.0")
+    }
+}
+
 subprojects {
     group = "to.dev.example"
     version = "1.0"
@@ -26,6 +38,18 @@ subprojects {
         implementation(kotlin("stdlib"))
         testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
         testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    }
+    val copyNativeDeps by tasks.creating(Copy::class) {
+        from(configurations.testRuntimeClasspath) {
+            include("*.dylib")
+            include("*.so")
+            include("*.dll")
+        }
+        into("$buildDir/libs")
+    }
+
+    tasks.test {
+        dependsOn(copyNativeDeps)
     }
 
     tasks.test {
